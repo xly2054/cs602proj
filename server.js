4a730d1a3ca16072bc160b416d0842209c8cd6e1
Xiaoling Yang <yangxiaoling@mail.com>
2018-02-27 20:14:50 -0500

    enable cors

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.engine('handlebars',
     handlebars({defaultLayout: 'main_logo'}));
 app.set('view engine', 'handlebars');
 
+//enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // static resources
 app.use(express.static(__dirname + '/public'));
 
